9b948e98cdd734b0fc8293aa7473ccd0dca80a86
wolf210210 <mithila210@gmail.com>
2018-11-29 14:21:38 +0530

    last

--- src/index.js
+++ src/index.js
@@ -1,27 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
 
 class App extends React.Component{
 
+    // constructor(props){
+    //     super(props);
 
-    constructor(props){
-        super(props);
-
-        this.state = {lat :null };
+    //     this.state = {lat :null ,errorMessage :'' };
         
+    // }
+
+    state = {lat :null ,errorMessage :'' };
+    
+    componentDidMount(){
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({lat:position.coords.latitude})
-            },
-            err => console.log(err)
-        );
+            position => this.setState({lat:position.coords.latitude}),
+            err => this.setState({errorMessage:err.message})
+            );
     }
 
+    // componentDidUpdate(){
+    //     console.log('My componet wes just update -it rerendered ')
+    // }
+
+    renderContent(){
+
+        if(this.state.errorMessage && !this.state.lat){
+            return (<div> Error : {this.state.errorMessage} </div>) 
+        }
+ 
+        if(!this.state.errorMessage && this.state.lat){
+            return <SeasonDisplay lat={this.state.lat}  />
+        }
+
+        return <Spinner message ="Please accept location request" />
+    }
+     
     render(){
-
-        return <div>Latitude:{this.state.lat} </div>;
+       return  <div className="border red"> {this.renderContent()} </div>
+     
     }
-
 }
 
 ReactDOM.render(
